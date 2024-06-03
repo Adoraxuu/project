e54a44e8e68e937b8522ec56d0ad80f45647f276
Adoraxuu <adoraxu01@gmail.com>
2024-06-03 18:24:50 +0800

    fix: comment

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from .forms import CommentForm
 from .models import Post, Comment
 from taggit.models import Tag
 
